06418893a794980873d7181908004900e8954167
Jerod Hatley <JHatley@Velosio.com>
2024-10-30 22:12:05 -0600

    init

--- function_app.py
+++ function_app.py
@@ -1,5 +1,10 @@
 import azure.functions as func
 import logging
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -7,19 +12,23 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def SyncToBlobFunction(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    report_uuid = req.params.get('report_uuid')
+    if not report_uuid:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            report_uuid = req_body.get('report_uuid')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if report_uuid:
+        data = getDataFromBlancco(report_uuid)
+        return func.HttpResponse(f"Data: {data}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a report_uuid in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+def getDataFromBlancco(report_uuid):
+    return f"Data {report_uuid} from Blancco"
